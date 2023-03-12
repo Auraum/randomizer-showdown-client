8c98e617d10aefc5a703741e8ba618bdde51470d
Auraum <aldomi04@ucm.es>
2023-03-12 16:10:21 +0100

    client battle

--- js/client-battle.js
+++ js/client-battle.js
@@ -638,6 +638,8 @@
 				var currentlyDynamaxed = (!canDynamax && maxMoves);
 				for (var i = 0; i < curActive.moves.length; i++) {
 					var moveData = curActive.moves[i];
+					if (this.battle.tier.includes("Ice Platinum")) this.battle.dex = Dex.mod('gen4iceplatinum');
+					if (this.battle.tier.includes("White Illusion")) this.battle.dex = Dex.mod('gen5whiteillusion');
 					var move = this.battle.dex.moves.get(moveData.move);
 					var name = move.name;
 					var pp = moveData.pp + '/' + moveData.maxpp;
